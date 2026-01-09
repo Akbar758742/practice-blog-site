64842efaef462db35de3ca6a4bb765ac9a5298f3
Akbar758742 <mdakbar758742@gmail.com>
2026-01-09 22:25:58 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::view('/example-auth', 'example-auth');
  */
 
 Route::prefix('admin')->name('admin.')->group(function () {
-    Route::middleware(['guest'])->group(function () {
+    Route::middleware(['guest','preventBackHistory'])->group(function () {
         Route::controller(AuthController::class)->group(function () {
             Route::get('/login', 'loginForm')->name('login');
             Route::get('/forget-password', 'forgetPassword')->name('forgetPassword');
@@ -36,7 +36,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
         });
     });
 
-    Route::middleware(['auth'])->group(function () {
+    Route::middleware(['auth','preventBackHistory'])->group(function () {
         Route::controller(AdminController::class)->group(function () {
             Route::get('/dashboard', 'AdminDashboard')->name('dashboard');
               Route::post('/logout', 'logoutHandler')->name('logout');
